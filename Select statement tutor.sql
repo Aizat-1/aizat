a64eb05b33913fdbbd09909b6dfb37233bf98a9a
Aizat-1 <136517127+Aizat-1@users.noreply.github.com>
2025-12-07 13:08:29 +0800

    Add files via upload

---
+++ Select statement tutor.sql
@@ -0,0 +1,15 @@
+SELECT * 
+FROM parks_an d_recreation.employee_demographics;
+
+
+
+SELECT employee_id , first_name, age, (age + 10) * 10 + 10
+FROM parks_and_recreation.employee_demographics;
+# PEMDAS
+
+SELECT DISTINCT FIRST_NAME, gender
+FROM parks_and_recreation.employee_demographics;
+
+
+
+
